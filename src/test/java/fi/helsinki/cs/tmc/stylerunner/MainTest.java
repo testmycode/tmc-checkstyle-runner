ea98e42d96c700b123f61ce66d44c568b073c1b7
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-05-15 13:36:06 +0300

    Structure for testing

---
+++ src/test/java/fi/helsinki/cs/tmc/stylerunner/MainTest.java
@@ -0,0 +1,14 @@
+package fi.helsinki.cs.tmc.stylerunner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void youShallPass() {
+
+        assertTrue(true);
+    }
+}
