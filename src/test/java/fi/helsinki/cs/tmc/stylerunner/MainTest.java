b3d2b7b2d84b7be404e1ba22ddf3ffff24d7330e
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-05-15 14:25:33 +0300

    Test template

--- src/test/java/fi/helsinki/cs/tmc/stylerunner/MainTest.java
+++ src/test/java/fi/helsinki/cs/tmc/stylerunner/MainTest.java
@@ -1,14 +1,37 @@
 package fi.helsinki.cs.tmc.stylerunner;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class MainTest {
+public final class MainTest {
+
+    private final ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream stderr = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+
+        System.setOut(new PrintStream(stdout));
+        System.setErr(new PrintStream(stderr));
+    }
+
+    @After
+    public void tearDown() {
+
+        System.setOut(null);
+        System.setErr(null);
+    }
 
     @Test
-    public void youShallPass() {
+    public void shouldPrintUsageOnNoArguments() {
 
-        assertTrue(true);
+        System.out.println("Hello");
+        assertEquals("Hello\n", stdout.toString());
     }
 }
