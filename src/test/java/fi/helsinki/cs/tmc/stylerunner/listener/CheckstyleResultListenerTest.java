3e27ae57b8201f9d4d98bf9be65031631e43efca
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2014-05-20 20:06:39 +0300

    Code style

--- src/test/java/fi/helsinki/cs/tmc/stylerunner/listener/CheckstyleResultListenerTest.java
+++ src/test/java/fi/helsinki/cs/tmc/stylerunner/listener/CheckstyleResultListenerTest.java
@@ -2,7 +2,9 @@ package fi.helsinki.cs.tmc.stylerunner.listener;
 
 import com.puppycrawl.tools.checkstyle.api.AuditEvent;
 import com.puppycrawl.tools.checkstyle.api.LocalizedMessage;
+
 import fi.helsinki.cs.tmc.stylerunner.validation.ValidationError;
+
 import java.io.File;
 import java.util.Collection;
 import java.util.List;
@@ -10,8 +12,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeSet;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -20,6 +24,51 @@ public class CheckstyleResultListenerTest {
     private CheckstyleResultListener resultListener;
     private final AuditEvent audit = createNewAuditEvent("tiedosto", 0, 1, "viesti", "testi");
     private final AuditEvent audit2 = createNewAuditEvent("tiedosto2", 1, 1, "Viesti 2", "test2");
+
+    private void containsErrors(final AuditEvent... errors) {
+
+        final Map<File, List<ValidationError>> validationErrors = this.resultListener.getResult().getValidationErrors();
+
+        assertEquals(errors.length, countErrorsFromMap(validationErrors));
+
+        final Set<String> files = new TreeSet<String>();
+
+        for (final AuditEvent error : errors) {
+            files.add(error.getFileName());
+            assertTrue(containsError(validationErrors, error));
+        }
+
+        assertEquals(files.size(), validationErrors.size());
+    }
+
+    private int countErrorsFromMap(final Map<File, List<ValidationError>> validationErrors) {
+
+        int errors = 0;
+
+        for (final Collection<ValidationError> errorCollection : validationErrors.values()) {
+            errors += errorCollection.size();
+        }
+
+        return errors;
+    }
+
+    private boolean containsError(final Map<File, List<ValidationError>> validationErrors, final AuditEvent error) {
+
+        for (Entry<File, List<ValidationError>> entry : validationErrors.entrySet()) {
+
+            for (ValidationError validationError : entry.getValue()) {
+
+                if (validationError.getSourceName().equals(error.getSourceName()) &&
+                    validationError.getLine() == error.getLine() &&
+                    validationError.getColumn() == error.getColumn() &&
+                    validationError.getSourceName().equals(error.getSourceName())) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 
     @Before
     public void setUp() {
@@ -45,6 +94,7 @@ public class CheckstyleResultListenerTest {
 
         resultListener.addError(audit);
         resultListener.addError(audit2);
+
         containsErrors(audit, audit2);
     }
 
@@ -53,6 +103,7 @@ public class CheckstyleResultListenerTest {
 
         resultListener.addError(audit);
         resultListener.addError(audit);
+
         containsErrors(audit, audit);
     }
 
@@ -63,48 +114,23 @@ public class CheckstyleResultListenerTest {
         resultListener.addError(audit);
         resultListener.addError(audit2);
         resultListener.addError(audit2);
+
         containsErrors(audit, audit, audit2, audit2);
     }
 
-    private AuditEvent createNewAuditEvent(final String filename, final int lineNumber, final int columnNumber, final String message, final String sourceName) {
+    private AuditEvent createNewAuditEvent(final String filename,
+                                           final int lineNumber,
+                                           final int columnNumber,
+                                           final String message,
+                                           final String sourceName) {
 
-        return new AuditEvent(sourceName, filename, new LocalizedMessage(lineNumber, columnNumber, null, null, null, null, String.class, message));
-    }
-
-    private void containsErrors(final AuditEvent... errors) {
-
-        final Map<File, List<ValidationError>> validationErrors = this.resultListener.getResult().getValidationErrors();
-
-        assertEquals(errors.length, countErrorsFromMap(validationErrors));
-
-        final Set<String> files = new TreeSet<String>();
-        for (final AuditEvent error : errors) {
-            files.add(error.getFileName());
-            assertTrue(containsError(validationErrors, error));
-        }
-        assertEquals(files.size(), validationErrors.size());
-    }
-
-    private int countErrorsFromMap(final Map<File, List<ValidationError>> validationErrors) {
-
-        int errors = 0;
-        for (final Collection<ValidationError> errorCollection : validationErrors.values()) {
-            errors += errorCollection.size();
-        }
-        return errors;
-    }
-
-    private boolean containsError(final Map<File, List<ValidationError>> validationErrors, final AuditEvent error) {
-        for (Entry<File, List<ValidationError>> entry : validationErrors.entrySet()) {
-            for (ValidationError validationError : entry.getValue()) {
-                if (validationError.getSourceName().equals(error.getSourceName()) &&
-                        validationError.getLine() == error.getLine() &&
-                        validationError.getColumn() == error.getColumn() &&
-                        validationError.getSourceName().equals(error.getSourceName())) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return new AuditEvent(sourceName, filename, new LocalizedMessage(lineNumber,
+                                                                         columnNumber,
+                                                                         null,
+                                                                         null,
+                                                                         null,
+                                                                         null,
+                                                                         String.class,
+                                                                         message));
     }
 }
